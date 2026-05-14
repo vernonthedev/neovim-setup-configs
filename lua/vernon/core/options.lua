faff6494697fb650a02d3042d87667d367d552b0
vernonthedev <techjaja2@gmail.com>
2026-05-14 10:26:04 +0300

    chore: added open newtab func, bt has a bug on tab duplications

--- lua/vernon/core/options.lua
+++ lua/vernon/core/options.lua
@@ -45,3 +45,47 @@ opt.swapfile = false
 
 -- Required for markdown symbols and images to render properly
 vim.opt.conceallevel = 2
+
+-- Enable mouse support in all modes
+vim.opt.mouse = "a" 
+
+-- Add "Open in New Tab" to the right-click context menu
+-- 2. Create a smart global function to resolve the file path under the mouse
+_G.open_right_clicked_node_in_tab = function()
+  -- Force Neovim to register the mouse position and click the sidebar node first
+  vim.api.nvim_input("<LeftMouse>")
+  
+  -- Give Neovim a tiny microsecond to update its internal cursor position
+  vim.schedule(function()
+    local bufnr = vim.api.nvim_get_current_buf()
+    local ft = vim.bo[bufnr].filetype
+
+    -- IF WE ARE IN NEO-TREE: Extract the actual file path from the hovered node
+    if ft == "neo-tree" then
+      local success, state = pcall(require, "neo-tree.sources.manager")
+      if success then
+        -- Get the current state of the filesystem source
+        local fs_state = state.get_state("filesystem")
+        if fs_state and fs_state.tree then
+          local node = fs_state.tree:get_node()
+          if node and node.type ~= "directory" then
+            -- Safely open the real file path in a new tab
+            vim.cmd("tabedit " .. vim.fn.fnameescape(node.path))
+            return
+          end
+        end
+      end
+    end
+
+    -- IF WE ARE IN A NORMAL FILE: Just open the file normally
+    if ft ~= "neo-tree" and ft ~= "" then
+      vim.cmd("tabedit %")
+    end
+  end)
+end
+
+-- 3. Map the right-click menu item to our smart Lua function
+-- We use a silent call so it doesn't flash commands on your screen
+vim.cmd([[
+  anoremenu PopUp.Open\ in\ New\ Tab :lua _G.open_right_clicked_node_in_tab()<CR>
+]])
