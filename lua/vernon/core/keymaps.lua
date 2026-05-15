a0678d6001cfb597b0bc381eb6b4fa40db2b47b7
vernonthedev <techjaja2@gmail.com>
2026-05-15 11:41:06 +0300

    feat: added keyboard shortcuts for quick close

--- lua/vernon/core/keymaps.lua
+++ lua/vernon/core/keymaps.lua
@@ -30,3 +30,8 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+
+-- Quick quit all
+vim.keymap.set("n", "<leader>qq", "<cmd>qa<CR>", { desc = "Quit all channels/windows" })
+vim.keymap.set("n", "<leader>qw", "<cmd>wqa<CR>", { desc = "Save all and quit" })
+vim.keymap.set("n", "<leader>qf", "<cmd>qa!<CR>", { desc = "Force quit all (No save)" })
